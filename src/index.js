5335d0c4ebdc58824680703ed429fb21ae377fa7
KaterinaValiuk <e5054404@gmail.com>
2021-08-19 00:46:44 +0300

    docs: Started the taks. Don't work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+/*jshint esversion: 8 */
+
 module.exports = function reverse (n) {
-  
-}
+    const a = (Math.abs(n)).toString();
+    let result =  '';
+    let char = 0;
+    const isNegative = n < 0;
+    
+    for (let i = 0; i < a.length; i++) {
+      let char = a[i];}
+      if (char !== '0') {result = char + result;}
+      else if (char === '0' && a[i + 1] && a[i + 1] !== '0') {res = char + result;}
+    
+      return Number(isNegative ? result * -1 : result);
+  }
+
